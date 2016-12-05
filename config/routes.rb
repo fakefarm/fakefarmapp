15a7fd53d569e7e07d94576647ae43628a0d9de4
woodall <dave@woodalls.me>
2016-12-05 15:11:50 -0500

    Update route for wwwoodall

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-  get 'wwwoodall/index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'wwwoodall' => 'wwwoodall#index'
 end
--- test/controllers/wwwoodall_controller_test.rb
+++ test/controllers/wwwoodall_controller_test.rb
@@ -2,8 +2,7 @@ require 'test_helper'
 
 class WwwoodallControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get wwwoodall_index_url
+    get wwwoodall_url
     assert_response :success
   end
-
 end
