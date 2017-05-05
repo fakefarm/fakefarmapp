4f91efab8a8a01aaad36689bc874359f47b5f357
wwwoodall <dave@woodalls.me>
2017-05-05 11:11:41 -0400

    remove unused test files

--- test/decorators/tickets_decorator_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class TicketsDecoratorTest < ActiveSupport::TestCase
-  test "these are real" do
-    tix = [tickets(:one), tickets(:two)]
-    td = TicketsDecorator.new(tix)
-    assert_equal td.count, 2
-  end
-end
--- test/models/social_network_service_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-require 'social_network_service'
-
-class SocialNetworkServiceTest < ActiveSupport::TestCase
-  test "get all social networks" do
-    s = SocialNetworkService.new.all
-    assert_equal 6, s.count 
-  end
-end
