21fafc58da76d03f8172cc219f091326e406f384
wwwoodall <dave@woodalls.me>
2017-05-09 10:59:41 -0400

    TODO - learn some capybara

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -1,9 +1,16 @@
 require "application_system_test_case"
 
 class UsersTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit users_url
-  #
-  #   assert_selector "h1", text: "User"
-  # end
+  test "visiting the index" do
+    visit users_url
+
+    assert_selector "h1", text: "User"
+  end
+
+  test 'New User signup' do
+    visit signup_path
+    assert_selector "h1", text: "Join the Farm!"
+    # _dw TODO learn some capybara to know how to do these kinds of things
+    # fill_in 'name' with: 'don@mattingly.com'
+  end
 end
