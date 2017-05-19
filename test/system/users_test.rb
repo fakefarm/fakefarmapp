5a8e28c11b14dccbbe9ac9259eedfe29556c6230
wwwoodall <dave@woodalls.me>
2017-05-19 17:04:21 -0400

    Users System Test
    Feels good to get started with Users System Testing.

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -3,14 +3,35 @@ require "application_system_test_case"
 class UsersTest < ApplicationSystemTestCase
   test "visiting the index" do
     visit users_url
-
     assert_selector "h1", text: "User"
   end
 
-  test 'New User signup' do
-    visit signup_path
-    assert_selector "h1", text: "Join the Farm!"
-    # _dw TODO learn some capybara to know how to do these kinds of things
-    # fill_in 'name' with: 'don@mattingly.com'
+  describe 'New User signup flow' do
+    test 'flow from root path to show page' do
+      visit root_path
+      click_link 'join'
+      enter_and_sumbit_form_data
+      assert_selector "#notice", text: 'User was successfully created.'
+    end
+
+    test 'content on the new user page' do
+      visit new_user_path
+      assert_selector "h1", text: "Join the Farm!"
+    end
+
+    test 'content on the show page after signing up' do
+      visit new_user_path
+      enter_and_sumbit_form_data
+      assert_selector "p", text: "don mattingly"
+      assert_selector "p", text: "don@mattingly.com"
+    end
+  end
+
+  def enter_and_sumbit_form_data
+    fill_in 'Name', with: 'don mattingly'
+    fill_in 'Email', with: 'don@mattingly.com'
+    fill_in 'Password', with: 'letmein'
+    fill_in 'Password confirmation', with: 'letmein'
+    click_button 'Create User'
   end
 end
