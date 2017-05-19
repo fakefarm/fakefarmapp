73c13244e09db8cb2ca68f80b029f93324ac6f26
wwwoodall <dave@woodalls.me>
2017-05-19 19:54:17 -0400

    wip

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,9 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      flash.now[:notice] = 'Welcome back jack'
-      redirect_to user_path(user)
+      flash[:notice] = 'Welcome back jack'
+      log_in user
+      redirect_to user
     else
       flash.now[:notice] = 'Invalid email/password combination'
       render :new
