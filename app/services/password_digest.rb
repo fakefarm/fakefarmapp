dee7326c0be2f226cced0cf0d5f0280322d98f91
wwwoodall <dave@woodalls.me>
2017-05-21 17:19:10 -0400

    PasswordDigest Service

---
+++ app/services/password_digest.rb
@@ -0,0 +1,6 @@
+module PasswordDigest
+  def self.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+end
