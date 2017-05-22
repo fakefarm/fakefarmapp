bde9a8eba74d6fa23b73326e65a7428191767cf8
wwwoodall <dave@woodalls.me>
2017-05-22 14:25:12 -0400

    Use Yarn
    This was a painless process. I installed yarn with brew then ran the
    initialize command which created package.json and yarn file.
    
    I added the basic js libs I've been hacking on; underscore, backbone,
    and jquery. So far, seems like yarn is a huge win!

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
+//= require jquery/dist/jquery.min
 //= require jquery_ujs
 //= require turbolinks
+//= require underscore/underscore-min
+//= require backbone/backbone-min
 //= require_tree .
