60e8236d602192c98de0ea58ec02759686a86555
wwwoodall <dave@woodalls.me>
2017-06-04 15:33:21 -0400

    Remove Scaffold comments

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,31 +1,23 @@
 class TagsController < ApplicationController
   before_action :set_tag, only: [:show, :edit, :update, :destroy]
 
-  # GET /tags
-  # GET /tags.json
   def index
     @tags = Tag.all
   end
 
-  # GET /tags/1
-  # GET /tags/1.json
   def show
     authorize @tag
   end
 
-  # GET /tags/new
   def new
     @tag = Tag.new
     authorize @tag
   end
 
-  # GET /tags/1/edit
   def edit
     authorize @tag
   end
 
-  # POST /tags
-  # POST /tags.json
   def create
     @tag = Tag.new(tag_params)
     authorize @tag
@@ -41,8 +33,6 @@ class TagsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /tags/1
-  # PATCH/PUT /tags/1.json
   def update
     authorize @tag
     respond_to do |format|
@@ -56,8 +46,6 @@ class TagsController < ApplicationController
     end
   end
 
-  # DELETE /tags/1
-  # DELETE /tags/1.json
   def destroy
     authorize @tag
     @tag.destroy
@@ -68,12 +56,10 @@ class TagsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_tag
       @tag = Tag.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def tag_params
       params.require(:tag).permit(:title, :body)
     end
