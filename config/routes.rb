2c439bf88512d255140ca842ab3aa7e4b6c85b8c
fakefarm <dave@woodalls.me>
2017-06-13 21:19:06 -0400

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,16 @@
 Rails.application.routes.draw do
-  resources :imgs
   mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
-
   mount ImgUploader::UploadEndpoint => '/imgs/upload'
-  resources :imgs
-  root to: 'imgs#index'
 
+  resources :imgs
+  resources :tags
+  resources :users
+
+  get    'signup' => 'users#new'
+  get    'about'  => 'pages#about'
   get    'login',  to: 'sessions#new'
   post   'login',  to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
-  get  'signup' => 'users#new'
-  resources :users
-  resources :tags
-
-  get 'signup' => 'users#new'
-  get 'about'  => 'pages#about'
-  # root 'pages#root'
+  root 'pages#root'
 end
